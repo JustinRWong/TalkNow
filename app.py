529722022e93b6093ae11fa7f4d73695bf179b2f
JustinRWong <justinryanwong@berkeley.edu>
2021-03-15 02:19:58 -0700

    included health check and set up folder structure

--- app.py
+++ app.py
@@ -1,6 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, Response, request, abort, jsonify, render_template, session
+from src.gateway import *
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('index.html', title='Home')
+
+@app.route('/start_meeting')
+def start_meeting():
     return render_template('base.html', title='Home')
+
+
+@app.route('/__healthcheck__', methods=['GET', 'POST'])
+def health_check():
+    '''
+    Health check responds for GET and POST
+    GET: returns time time of server receiving request.
+    POST: echos parameters and data passeed by request.
+    '''
+    ## Query parameter argumeents
+    query_string_params = request.args
+
+    ## data parameters from http request
+    data_params = request.get_json()
+    print(data_params)
+    if request.method == 'GET':
+        posted = { 'params': query_string_params, 'data': data_params}
+        return render_template( 'healthcheck.html',
+                                title='Healthcheck',
+                                ttr={'time of response': time.time(), 'date': datetime.now()},
+                                echo=posted)
+
+    if request.method == 'POST':
+        form_param = request.form['form_param']
+        posted = { 'params': query_string_params, 'data': data_params , 'form_param': form_param}
+        return render_template( 'healthcheck.html',
+                                title='Healthcheck',
+                                ttr={'time of response': time.time(), 'date': datetime.now()},
+                                echo=posted)
---
+++ src/gateway.py
@@ -0,0 +1,4 @@
+from flask_sqlalchemy import SQLAlchemy
+import requests
+import json, time, hashlib
+from datetime import datetime, timedelta
